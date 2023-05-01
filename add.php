b955942431fa5064d0ec312aac4cf0700ce54a41
rasika <rasikapatil3499@gmail.com>
2023-05-01 19:06:54 +0530

    add.php

--- add.php
+++ add.php
@@ -11,7 +11,7 @@ $query="select SUM(amount) as `sumamount` from recurring ";
 $res=mysqli_query($con,$query);
 $data =mysqli_fetch_array($res);
 
-echo "<h3> Total Amount: $data[sumamount]</h3>";
+echo "<h3> Total Amount: $data[sumamount]</h3> ";
 
 // Close connection
 mysqli_close($con);
