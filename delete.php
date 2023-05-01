840aae34a65c148e12a030d621899dffb8cb55b1
rasika <rasikapatil3499@gmail.com>
2023-05-01 18:54:21 +0530

    master

--- delete.php
+++ delete.php
@@ -1,22 +1,3 @@
 <?php
 
-include 'connection.php';
-
-if (isset($_GET['deleteid'])) {
-    $no = $_get['deleteid'];
-
-
-    $sql = "delete from `recurring` where id='.$no.'";
-    $result = mysqli_query($con, $sql);
-    if ($result) {
-        header('location:display.php');
-
-    } else {
-        die(mysqli_error($con));
-
-    }
-
-}
-
-
 ?>
